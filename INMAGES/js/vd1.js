c7e87d09096637e094a89ac5f2c5c07f0caa516f
quangteo134 <quangyt300@gmail.com>
2022-09-23 17:35:05 +0700

    upload

---
+++ INMAGES/js/vd1.js
@@ -0,0 +1,10 @@
+$(document).realy(function() {
+    $("#tinh-ma").hover(function () {
+            $(this).attr("src", "INMAGES/anh-avatar-halloween-dep-2_051557932.jpg");
+
+            
+        }, function () {
+            $(this).attr("src", "INMAGES/Ảnh chụp màn hình 2022-08-22 150001.jpg");
+        }
+    );
+}
